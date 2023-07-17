b47bc91e90328e8625f84520399beda529a17017
George <george.irlgames@gmail.com>
2023-07-17 19:51:33 +0800

    Audio Manager works

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -3,27 +3,102 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class AudioManager : MonoBehaviour
 {
-    private AudioSource audioSource;
-    public AudioClip clip;
+    public static AudioManager instance;
 
-   
-    // Start is called before the first frame update
-    void Start()
+    private string previousScene;
+    public AudioSource audioSource;
+
+    [System.Serializable]
+    public class SceneAudioClip
     {
-        audioSource = GetComponent<AudioSource>();
+        public string sceneName;
+        public AudioClip audioClip;
+    }
 
+    public List<SceneAudioClip> sceneAudioClips = new List<SceneAudioClip>();
+    private Dictionary<string, AudioClip> audioClipMap = new Dictionary<string, AudioClip>();
 
-        if (clip != null)
+    
+
+    //************************************************************************\\
+    private void Awake()
+    {
+        if (instance == null)
         {
-            audioSource.clip = clip;
-            audioSource.Play();
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        foreach (SceneAudioClip sceneAudioClip in sceneAudioClips)
+        {
+            audioClipMap[sceneAudioClip.sceneName] = sceneAudioClip.audioClip;
         }
     }
 
-    private void OnDestroy()
+    private void Start()
     {
-        audioSource.Stop();
+        string loadedScene = GameManager.loadedScene;
+        PlaySceneAudio(loadedScene);
+
+        previousScene = loadedScene;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        string currentScene = GameManager.loadedScene;
+        if (currentScene != previousScene)
+        {
+            PlaySceneAudio(currentScene);
+            previousScene = currentScene;
+        }
+    }
+
+    private void PlaySceneAudio(string sceneName)
+    {
+        if (audioClipMap.ContainsKey(sceneName))
+        {
+            AudioClip audioClip = audioClipMap[sceneName];
+            if (audioClip != null)
+            {
+                // Check for special cases where music continues between scenes
+                if (ShouldMusicContinue(previousScene, sceneName))
+                {
+                    // Music continues, no need to change the audio clip
+                    return;
+                }
+
+                // Stop the previous clip if it's playing
+                audioSource.Stop();
+
+                // Play the new clip
+                audioSource.PlayOneShot(audioClip);
+                return;
+            }
+        }
+
+        Debug.LogWarning("Audio clip not found for scene: " + sceneName);
+    }
+
+    private bool ShouldMusicContinue(string previousScene, string currentScene)
+    {
+        // Check the specific cases where music should continue between scenes
+        if ((previousScene == "Bedroom" && currentScene == "LoungeKitchen") ||
+            (previousScene == "LoungeKitchen" && currentScene == "Bedroom"))
+        {
+            return true;
+        }
+        if ((previousScene == "UniEntrance" && currentScene == "UniClassroom") ||
+            (previousScene == "UniClassroom" && currentScene == "UniEntrance"))
+        {
+            return true;
+        }
+        return false;
     }
 }
